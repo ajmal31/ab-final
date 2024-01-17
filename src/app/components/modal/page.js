ea3732fa654a055c3bbe2302a30ab08b89c0f6cf
ajmal31 <ajmalmuhammed846@gmail.com>
2024-01-18 02:47:36 +0530

    Feat : products listing ,viewing and searching V1

---
+++ src/app/components/modal/page.js
@@ -0,0 +1,75 @@
+
+import { Modal, TextContainer, Button } from '@shopify/polaris';
+
+const ProductModal = ({ data, onClose }) => {
+    const handleShow = () => onClose();
+
+    return (
+        <Modal
+            open
+            onClose={handleShow}
+
+        >
+             {/* Header section  */}
+            <div className='border-b '>
+              
+                <Modal.Section>
+
+
+                    <TextContainer  >
+                        <p className='my-1 text-xl'>{data.title}</p>
+                    </TextContainer>
+
+
+                </Modal.Section>
+            </div>
+
+
+            {/* Image section */}
+            <Modal.Section style={{ border: "none" }} >
+                <div className='flex items-center justify-center w-full h-full '>
+                    {/* <TextContainer> */}
+                    <img src={data?.image} className="w-2/4 h-2/6 " alt="product_image" />
+                    {/* </TextContainer> */}
+                </div>
+            </Modal.Section>
+
+            {/* Description section */}
+            <Modal.Section>
+                <div className=' px-7'>
+                    <div className='h-full w-full border-y-2 py-12 border-gray-600' >
+                        <TextContainer>
+                            <p className='text-3xl'>Description</p>
+                            <p>{data?.description}</p>
+                        </TextContainer>
+                    </div>
+
+                </div>
+            </Modal.Section>
+            {/* Rating section */}
+            <Modal.Section  >
+                <div className=' px-7'>
+                    <div className='h-full w-full  py-12 border-gray-600' >
+                        <TextContainer>
+                            <div className='mb-1' >
+                                <p className='text-3xl' >Rating:</p>
+                            </div>
+
+
+                            <span className='font-bold'>Rating:</span>{data.rating.rate}
+                            <br />
+                            <span className='font-bold'>Rated by:</span>{data.rating.count}
+
+                        </TextContainer>
+                    </div>
+
+                </div>
+            </Modal.Section>
+
+
+
+        </Modal>
+    );
+};
+
+export default ProductModal;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Table from './components/table/page'
+export default function Home() {
+  return (
+
+      <Table></Table>
+  
+
+
+  )
+}
